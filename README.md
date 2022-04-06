f8fe2950f423aa0b71efbee422484d2e3d52bb9c
Moath94 <60042791+NasserHobani@users.noreply.github.com>
2022-04-06 11:57:34 +0300

    ..

---
+++ README.md
@@ -0,0 +1,1 @@
+# Mndoobk_d
--- app/src/main/java/com/pointtopoint/lib/MyLib.java
+++ app/src/main/java/com/pointtopoint/lib/MyLib.java
@@ -1,4 +1,44 @@
 package com.pointtopoint.lib;
 
+import android.widget.SearchView;
+
+import com.pointtopoint.adapter.Adapter;
+import com.pointtopoint.model.OrderDataModel;
+
+import java.util.ArrayList;
+import java.util.Date;
+
 public class MyLib {
+
+    public long printDifference(Date startDate, Date endDate) {
+        //milliseconds
+        long different = endDate.getTime() - startDate.getTime();
+
+//        System.out.println("startDate : " + startDate);
+//        System.out.println("endDate : "+ endDate);
+//        System.out.println("different : " + different);
+
+        long secondsInMilli = 1000;
+        long minutesInMilli = secondsInMilli * 60;
+        long hoursInMilli = minutesInMilli * 60;
+        long daysInMilli = hoursInMilli * 24;
+//
+//        long elapsedDays = different / daysInMilli;
+//        different = different % daysInMilli;
+//
+//        long elapsedHours = different / hoursInMilli;
+//        different = different % hoursInMilli;
+
+        long elapsedMinutes = different / minutesInMilli;
+        different = different % minutesInMilli;
+
+//        long elapsedSeconds = different / secondsInMilli;
+
+//        System.out.printf(
+//                "%d days, %d hours, %d minutes, %d seconds%n",
+//                elapsedDays, elapsedHours, elapsedMinutes, elapsedSeconds);
+        return elapsedMinutes;
+
+    }
+
 }
